6ab1aedbfd2d0809c4480247ebd6dc8deefe980a
Rodrigo Morán <67351706+Rom1893@users.noreply.github.com>
2023-04-10 00:12:53 -0400

    Fixed scripts & model

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,15 @@
 const http = require("http");
 const app = require("./app"); // Express app from another file
 const PORT = process.env.PORT || 8000;
+const { loadPlanetsData } = require("./models/planets.model");
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
+const startServer = async () => {
+  await loadPlanetsData();
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+};
+
+startServer();
