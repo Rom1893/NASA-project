bfb7267b061879cf8a89e15eff006b4c32194df4
Rodrigo Morán <67351706+Rom1893@users.noreply.github.com>
2023-04-11 17:38:54 -0400

    error handling for launches

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -7,6 +7,24 @@ const httpGetAllLaunches = (req, res) => {
 const httpAddNewLaunch = (req, res) => {
   const launch = req.body;
 
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property",
+    });
+  }
+
+  launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Invalid launch date",
+    });
+  }
+
   launch.launchDate = new Date(launch.launchDate);
 
   addNewLaunch(launch);
