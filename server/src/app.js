44ae79723b2bb3fbd62145b820c5d25bdf5d9e6f
Rodrigo Morán <67351706+Rom1893@users.noreply.github.com>
2023-04-11 12:26:28 -0400

    New launches Route

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,24 @@
-const express = require("express");
-const cors = require("cors");
-const planetsRouter = require("./routes/planets/planets.router");
-const app = express();
 const path = require("path");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+
+const planetsRouter = require("./routes/planets/planets.router");
+const launchesRouter = require("./routes/launches/launches.router");
 
 app.use(
   cors({
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(planetsRouter);
+app.use(launchesRouter);
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
