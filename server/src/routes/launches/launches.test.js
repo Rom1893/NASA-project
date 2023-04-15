96707eb1f9baf5f670e51b613a9798299ebe4f08
Rodrigo Morán <67351706+Rom1893@users.noreply.github.com>
2023-04-14 22:35:51 -0400

    added jest post

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -4,3 +4,9 @@ describe("Test GET /launches", () => {
     expect(response).toBe(200);
   });
 });
+
+describe("Test Post /launch", () => {
+  test("It should respond with 200 success", () => {});
+  test("It should catch missing required properties", () => {});
+  test("It should catch invalid dates", () => {});
+});
