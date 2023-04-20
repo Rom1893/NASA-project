552f867e8a5ce781a8832c48ec099962d890d32f
Rodrigo Morán <67351706+Rom1893@users.noreply.github.com>
2023-04-20 00:37:08 -0400

    added mongo URL

--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 8000;
 const { loadPlanetsData } = require("./models/planets.model");
 
 const server = http.createServer(app);
+const MONGO_URL =
+  "mongodb+srv://rom1893:<password>@nasacluster.hlvqmua.mongodb.net/?retryWrites=true&w=majority";
 
 const startServer = async () => {
   await loadPlanetsData();
