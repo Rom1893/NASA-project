e963f617b49da3d397a29e1cf26225c7a7bc2223
Rodrigo Morán <67351706+Rom1893@users.noreply.github.com>
2023-04-21 15:54:28 -0400

    New launches schema

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,30 @@
 const http = require("http");
+const mongoose = require("mongoose");
 const app = require("./app"); // Express app from another file
 const PORT = process.env.PORT || 8000;
 const { loadPlanetsData } = require("./models/planets.model");
 
 const server = http.createServer(app);
 const MONGO_URL =
-  "mongodb+srv://rom1893:<password>@nasacluster.hlvqmua.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://@nasacluster.hlvqmua.mongodb.net/nasa?retryWrites=true&w=majority";
 
 const startServer = async () => {
+  mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
   });
 };
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
 
 startServer();
