5fd40c213792b4ed7299c330b68d496563121e69
Rodrigo Morán <67351706+Rom1893@users.noreply.github.com>
2023-04-23 01:02:40 -0400

    Upsert setup

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,6 +2,8 @@ const { parse } = require("csv-parse");
 const fs = require("fs");
 const path = require("path");
 
+const planets = require("./planets.mongo");
+
 const habitablePlanets = [];
 
 const isHabitablePlanet = (planet) => {
@@ -24,9 +26,11 @@ const loadPlanetsData = () => {
           columns: true,
         })
       )
-      .on("data", (data) => {
+      .on("data", async (data) => {
         if (isHabitablePlanet(data)) {
-          habitablePlanets.push(data);
+          // await planets.create({
+          //   keplerName: data.kepler_name,
+          // });
         }
       })
       .on("error", (err) => {
